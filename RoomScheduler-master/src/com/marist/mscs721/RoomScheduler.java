9108a5c59da6ed196a558802d766c95de65e7116
Your Name <kethirihimajareddy@gmail.com>
2016-02-25 14:26:24 -0500

    Issues fixed

--- RoomScheduler-master/src/com/marist/mscs721/RoomScheduler.java
+++ RoomScheduler-master/src/com/marist/mscs721/RoomScheduler.java
@@ -1,3 +1,9 @@
+/*
+ * @author Himaja
+ * @CWID 20062532
+ * @program RoomScheduler adds,removes and schedules rooms and these details 
+ * will be imported from or exported to JSON files
+ */
 package com.marist.mscs721;
 
 import org.apache.commons.io.FileUtils;
@@ -6,6 +12,7 @@ import java.io.*;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.logging.Logger;
 
 import org.json.*;
 
@@ -14,45 +21,59 @@ import au.com.bytecode.opencsv.CSVReader;
 public class RoomScheduler {
 
 	protected static Scanner keyboard = new Scanner(System.in);
+	private static final Logger logger = Logger
+			.getLogger("com.marist.mscs721.RoomScheduler");
 
 	// creating room scheduler based on room,meeting,scheduleroom methods
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		Boolean end = false;
 		ArrayList<Room> rooms = new ArrayList<Room>();
 		ArrayList<Meeting> meetings = new ArrayList<Meeting>();
-
+		// To perform operations on RoomSchedular by selecting a case
 		while (!end) {
 			switch (mainMenu()) {
 
 			case 1:
-				System.out.println(addRoom(rooms));
+				logger.info(addRoom(rooms));
 				break;
 			case 2:
-				System.out.println(removeRoom(rooms));
+				logger.info(removeRoom(rooms));
 				break;
 			case 3:
-				System.out.print(scheduleRoom(rooms));
+				logger.info(scheduleRoom(rooms));
 				break;
 			case 4:
-				System.out.println(listSchedule(rooms));
+				logger.info(listSchedule(rooms));
 				break;
 			case 5:
-				System.out.println(listRooms(rooms));
+				logger.info(listRooms(rooms));
 				break;
 			case 6:
-				System.out.println(saveRoomJSON(rooms));
+				logger.info(saveRoomJSON(rooms));
 				break;
 			case 7:
-				System.out.println(saveScheduleJSON(rooms));
+				logger.info(saveScheduleJSON(rooms));
 				break;
 			case 8:
 				// here i changed room to meeting because in method it is given
 				// meeting
-				System.out.println(saveMeetingJSON(meetings));
+				logger.info(saveMeetingJSON(meetings));
+
+				break;
+			case 9:
+
+				logger.info(importRoomJSON());
+
+				break;
+			case 10:
+				logger.info(importScheduleJSON());
+				break;
+			case 11:
+				logger.info(importMeetingJSON());
 				break;
 
 			default:
-				System.out.println("Invalid case selection");
+				logger.info("Invalid case selection");
 				break;
 			}
 
@@ -60,7 +81,9 @@ public class RoomScheduler {
 
 	}
 
-	// To print the list of scheduled rooms
+	/*
+	 * To print the list of scheduled rooms
+	 */
 	protected static String listSchedule(ArrayList<Room> roomList) {
 		String roomName = getRoomName();
 		System.out.println(roomName + " Schedule");
@@ -73,7 +96,9 @@ public class RoomScheduler {
 		return "";
 	}
 
-	// To print the results based on case selection
+	/*
+	 * To print the results based on selecting the case using console
+	 */
 	protected static int mainMenu() {
 		System.out.println("Main Menu:");
 		System.out.println("  1 - Add a room");
@@ -81,36 +106,54 @@ public class RoomScheduler {
 		System.out.println("  3 - Schedule a room");
 		System.out.println("  4 - List Schedule");
 		System.out.println("  5 - List Rooms");
-		System.out.println(" 6 - saveRoomJSON");
+		System.out.println(" 6 - save Roomdetails");
 		System.out.println(" 7 - save schedule");
 		System.out.println(" 8 - save meeting");
+		System.out.println(" 9 - import Roomdetails");
+		System.out.println(" 10 -import schedule");
+		System.out.println(" 11 -import meeting");
 		System.out.println("Enter your selection: ");
 
 		return keyboard.nextInt();
 	}
 
-	// To add rooms and checking the capacity of the rooms
+	/*
+	 * To add rooms and checking the capacity of the rooms if a room is not
+	 * available then print the unavailable message
+	 */
 	protected static String addRoom(ArrayList<Room> roomList) {
 		System.out.println("Add a room:");
 		String name = getRoomName();
 		System.out.println("Room capacity?");
 		int capacity = keyboard.nextInt();
-
 		Room newRoom = new Room(name, capacity);
 		roomList.add(newRoom);
-
+		if (capacity == 0)
+			System.out.println("");
 		return "Room '" + newRoom.getName() + "' added successfully!";
+
 	}
 
-	// To remove the rooms
+	/*
+	 * To remove the rooms from the room list which we have added using
+	 * addRooms() method
+	 */
 	protected static String removeRoom(ArrayList<Room> roomList) {
 		System.out.println("Remove a room:");
-		roomList.remove(findRoomIndex(roomList, getRoomName()));
 
-		return "Room removed successfully!";
+		int index = findRoomIndex(roomList, getRoomName());
+
+		if (index != 0) {
+			roomList.remove(index);
+			return "Room removed successfully";
+		} else {
+			return "Room doesn't exist!";
+		}
 	}
 
-	// To check the capacity of the room
+	/*
+	 * To print the list of rooms which we have added using addRooms() method
+	 */
 	protected static String listRooms(ArrayList<Room> roomList) {
 		System.out.println("Room Name - Capacity");
 		System.out.println("---------------------");
@@ -124,69 +167,91 @@ public class RoomScheduler {
 		return roomList.size() + " Room(s)";
 	}
 
-	// Schedule the room if it is available
+	/*
+	 * Schedule the rooms based on availability
+	 */
 
 	protected static String scheduleRoom(ArrayList<Room> roomList) {
-		System.out.println("Schedule a room:");
-		String name = getRoomName();
+		if (roomList.size() > 0) {
+			System.out.println("Schedule a room:");
+			String name = getRoomName();
+			if (findRoomIndex(roomList, name) != -1) {
+				System.out.println("Start Date? (yyyy-mm-dd):");
+				String startDate = keyboard.next();
+				System.out.println("Start Time?");
+				String startTime = keyboard.next();
+				startTime = startTime + ":00.0";
 
-		System.out.println("Start Date? (yyyy-mm-dd):");
-		String startDate = keyboard.next();
-		System.out.println("Start Time?");
-		String startTime = keyboard.next();
-		startTime = startTime + ":00.0";
+				System.out.println("End Date? (yyyy-mm-dd):");
+				String endDate = keyboard.next();
+				System.out.println("End Time?");
+				String endTime = keyboard.next();
+				endTime = endTime + ":00.0";
 
-		System.out.println("End Date? (yyyy-mm-dd):");
-		String endDate = keyboard.next();
-		System.out.println("End Time?");
-		String endTime = keyboard.next();
-		endTime = endTime + ":00.0";
+				Timestamp startTimestamp = Timestamp.valueOf(startDate + " "
+						+ startTime);
+				Timestamp endTimestamp = Timestamp.valueOf(endDate + " "
+						+ endTime);
 
-		Timestamp startTimestamp = Timestamp.valueOf(startDate + " "
-				+ startTime);
-		Timestamp endTimestamp = Timestamp.valueOf(endDate + " " + endTime);
+				System.out.println("Subject?");
+				String subject = keyboard.next();
 
-		System.out.println("Subject?");
-		String subject = keyboard.next();
+				Room curRoom = getRoomFromName(roomList, name);
 
-		Room curRoom = getRoomFromName(roomList, name);
-
-		Meeting meeting = new Meeting(startTimestamp, endTimestamp, subject);
-		// check weather time is already exists for timestamp
-		curRoom.addMeeting(meeting);
-
-		return "Successfully scheduled meeting!";
+				Meeting meeting = new Meeting(startTimestamp, endTimestamp,
+						subject);
+				// check wheather time is already exists for timestamp
+				curRoom.addMeeting(meeting);
+				return "Successfully scheduled meeting!";
+			} else {
+				return "Room " + name + " is not available in the added rooms";
+			}
+		} else {
+			return "No room is available for schedule";
+		}
 	}
 
+	/*
+	 * To get the room name by using getName() and comparing whether the room
+	 * name is null or not
+	 */
 	protected static Room getRoomFromName(ArrayList<Room> roomList, String name) {
 		return roomList.get(findRoomIndex(roomList, name));
 	}
 
 	protected static int findRoomIndex(ArrayList<Room> roomList, String roomName) {
 		int roomIndex = 0;
-
+		int finalIndex = 0;
+		boolean exist = false;
 		for (Room room : roomList) {
-			if (room.getName().compareTo(roomName) == 0) {
-				break;
-			}
-			roomIndex++;
-		}
+			if (room.getName().compareTo(roomName) == 0 && exist == true) {
 
-		return roomIndex;
+				exist = true;
+
+				roomIndex++;
+			}
+
+			if (!exist) {
+				roomIndex++;
+			}
+		}
+		if (exist) {
+			finalIndex = roomIndex;
+		}
+		return finalIndex;
 	}
 
+	/*
+	 * Takes input from the console and prints the room name
+	 */
 	protected static String getRoomName() {
 		System.out.println("Room Name?");
 		return keyboard.next();
 	}
 
-	// To save the room name to the csv in tabular format
-	protected static String saveToCsv() {
-		System.out.println("Room Name?");
-		return keyboard.next();
-	}
-
-	// Exporting the room details to the JSON files
+	/*
+	 * Exporting the room details to the JSON files
+	 */
 	protected static String saveRoomJSON(ArrayList<Room> roomList) {
 		JSONArray jsonArray = new JSONArray();
 
@@ -206,11 +271,13 @@ public class RoomScheduler {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		return "it is priting this one";
+		return "Successfully imported room details";
 
 	}
 
-	// Exporting the Scheduling room details to the JSON files
+	/*
+	 * Exporting the Scheduling room details to the JSON files
+	 */
 	protected static String saveScheduleJSON(ArrayList<Room> roomList) {
 		JSONArray jsonArray = new JSONArray();
 
@@ -241,7 +308,9 @@ public class RoomScheduler {
 
 	}
 
-	// Exporting the meeting details to JSON files
+	/*
+	 * Exporting the meeting details to JSON files
+	 */
 	protected static String saveMeetingJSON(ArrayList<Meeting> meetings) {
 		JSONArray jsonArray = new JSONArray();
 
@@ -264,14 +333,16 @@ public class RoomScheduler {
 
 			e.printStackTrace();
 		}
-		return "KK";
+		return "exported data successfully";
 	}
 
-	// Importing the meeting details from JSON files
-	protected static ArrayList<Meeting> importMeetingJSON(String filePath)
-			throws Exception {
+	/*
+	 * Importing the meeting details from JSON files
+	 */
+	protected static String importMeetingJSON() throws Exception {
 		// check for a valid file path
-		CSVReader readMeeting = new CSVReader(new FileReader(filePath));
+		CSVReader readMeeting = new CSVReader(
+				new FileReader("D://fromJSON.csv"));
 		ArrayList<Meeting> meetings = new ArrayList<Meeting>();
 		String[] nextLine;
 		// ignoring first line
@@ -286,16 +357,18 @@ public class RoomScheduler {
 		}
 		readMeeting.close();
 
-		return meetings;
+		return "";
 
 	}
 
-	// Importing the scheduling details from JSON files
+	/*
+	 * Importing the scheduling details from JSON files
+	 */
 
-	protected static ArrayList<Room> importScheduleJSON(String filePath)
-			throws Exception {
+	protected static String importScheduleJSON() throws Exception {
 		// check for a valid file path
-		CSVReader readMeeting = new CSVReader(new FileReader(filePath));
+		CSVReader readMeeting = new CSVReader(
+				new FileReader("D://fromJSON.csv"));
 		ArrayList<Room> rooms = new ArrayList<Room>();
 		String[] nextLine;
 		// ignoring first line
@@ -313,15 +386,17 @@ public class RoomScheduler {
 
 		}
 		readMeeting.close();
-		return rooms;
+		return "";
 
 	}
 
-	// Importing the room details from JSON files
-	protected static ArrayList<Room> importRoomJSON(String filePath)
-			throws Exception {
+	/*
+	 * Importing the room details from JSON files
+	 */
+	protected static String importRoomJSON() throws Exception {
 		// check for a valid file path
-		CSVReader readMeeting = new CSVReader(new FileReader(filePath));
+		CSVReader readMeeting = new CSVReader(new FileReader(
+				"D://roomfromJSON.csv"));
 		ArrayList<Room> roomList = new ArrayList<Room>();
 		String[] nextLine;
 		// ignoring first line
@@ -331,7 +406,7 @@ public class RoomScheduler {
 			roomList.add(newRoom);
 		}
 		readMeeting.close();
-		return roomList;
+		return "";
 
 	}
 
